2a47be31cac9b16abbe218599eb4170d1bdd6492
Victor Novais <victor@digitalrepublic.com.br>
2023-02-13 09:28:00 -0300

    feat/text-input e estilos

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Button,
+  Text,
+  TextInput,
+  View,
+  Platform
+} from 'react-native';
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mensagem: ''
+    }
+  }
+
+  mudouMensagem = msg => {
+    this.setState({ mensagem: msg });
+  }
+
+  render() {
+    const { mensagem } = this.state
+
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'android' ? (
+          <Text>Android</Text>
+        ) : (
+          <Text style={styles.text}>FIAP</Text>
+        )}
+        <Text style={[styles.text, styles.textColor]}>portal do aluno</Text>
+        <TextInput
+          placeholder='Login'
+          style={styles.textInput}
+          onChangeText={this.mudouMensagem}
+        />
+        <TextInput
+          placeholder='Password'
+          style={styles.textInput}
+        />
+        <Button
+          title="Clique-me"
+          onPress={() => alert(mensagem)}
+          color="red"
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +58,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 25,
+    ...Platform.select({
+      ios: {
+        color: 'red'
+      },
+      android: {
+        color: 'green'
+      }
+    })
+  },
+  textColor: {
+    color: 'gray'
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: 'gray',
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginTop: 10,
+    width: 200
+  }
 });
